5260105b557e21d9ac4e7caef8f58b51b44b71b2
Noah <noahschnur@gmail.com>
2021-07-06 19:00:40 -0700

    fixed link errors

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 
 function Nav(props) {
   let history = useHistory();
-  const {setContactSelected } = props;
+  const { setContactSelected } = props;
   return (
     <header id="App-header" className="container">
       <div className="row">
@@ -14,32 +14,50 @@ function Nav(props) {
           <nav id="App-nav">
             <ul>
               <li id="nav-li">
-                <a data-testid="about" href="/" onClick={() => {
-                  setContactSelected(false)
-                  history.push("/")
-                }}
-                id="App-link">
+                <a
+                  data-testid="about"
+                  onClick={() => {
+                    setContactSelected(false);
+                    history.push("/");
+                  }}
+                  id="App-link"
+                >
                   About Me
                 </a>
               </li>
               <li id="nav-li">
-                <a data-testid="portfolio" href="/portfolio" onClick={() => {
-                  setContactSelected(false)
-                  history.push("/Portfolio")}}
-                  id="App-link">
-                    Portfolio
+                <a
+                  data-testid="portfolio"
+                  onClick={() => {
+                    setContactSelected(false);
+                    history.push("/portfolio");
+                  }}
+                  id="App-link"
+                >
+                  Portfolio
                 </a>
               </li>
               <li id="nav-li">
-                <a data-testid="resume" href="/resume" onClick={() => {
-                  setContactSelected(false)
-                  history.push("/Resume")}}
-                  id="App-link">
-                    Resume
+                <a
+                  data-testid="resume"
+                  onClick={() => {
+                    setContactSelected(false);
+                    history.push("/resume");
+                  }}
+                  id="App-link"
+                >
+                  Resume
                 </a>
               </li>
               <li id="nav-li">
-                <a data-testid="contact" href="/#contact" onClick={() => setContactSelected(true)} id="App-link">
+                <a
+                  data-testid="contact"
+                  onClick={() => {
+                    setContactSelected(true);
+                    history.push("/contact");
+                  }}
+                  id="App-link"
+                >
                   Contact
                 </a>
               </li>
